af3bed77109247666bb36b42edfc376cbfff9634
Tushar Singh <149937568+Tushar-Deshwal-tech@users.noreply.github.com>
2024-07-13 18:26:48 +0000

    create footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./css/Footer.css";
 import logo from "../logo-150x122.png";
+import payment from "../icons/footer-payment-image.png"
 
 function Footer() {
   return (
     <div className="footer-main">
-      <div className="footer-upper-line"></div>
+      <div className="footer-upper-line">HOMEGROWN INDIAN BRAND</div>
       <div className="footer-content">
         <div className="first-content">
           <div>
@@ -18,34 +19,41 @@ function Footer() {
           <div></div>
         </div>
         <div className="second-content">
-          <div className="table-content">
+          <div className="footer-table-content">
             <table>
-              <tr>
-                <td>Need Help</td>
-                <td>About Company</td>
-                <td>Useful Links</td>
-              </tr>
-              <tr>
-                <td>FAQs</td>
-                <td>My Account</td>
-                <td>Contact Us</td>
-              </tr>
-              <tr>
-                <td>Men</td>
-                <td>Women</td>
-                <td>About Us</td>
-              </tr>
-              <tr>
-                <td>Terms of Use</td>
-                <td>Privacy Policy</td>
-                <td>Return Policy</td>
-              </tr>
+              <thead>
+                <tr>
+                  <th>Need Help</th>
+                  <th>About Company</th>
+                  <th>Useful Links</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>FAQs</td>
+                  <td>My Account</td>
+                  <td>Contact Us</td>
+                </tr>
+                <tr>
+                  <td>Men</td>
+                  <td>Women</td> 
+                  <td>About Us</td>
+                </tr>
+                <tr>
+                  <td>Terms of Use</td>
+                  <td>Privacy Policy</td>
+                  <td>Return Policy</td>
+                </tr>
+              </tbody>
             </table>
+            <div className="payment-section">
+              <div>100% Secure Payment:</div>
+              <div><img src={payment} alt="Secure Payment" className="payment-img" /></div>
+            </div>
           </div>
-          <div>100% Secure Payment:</div>
         </div>
       </div>
-      <div className="footer-lower-line"></div>
+      <div className="footer-lower-line">©2024. Solo Site. All Rights Reserved. <span className="footer-lower-V-line"></span></div>
     </div>
   );
 }
