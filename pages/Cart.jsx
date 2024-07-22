3df7df5d6d7e9b529486917522ca691428b8eb77
Tushar Singh <149937568+Tushar-Deshwal-tech@users.noreply.github.com>
2024-07-22 13:13:45 +0530

    Update Cart.jsx
    add cart layout templates

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -6,23 +6,41 @@ function Cart() {
     const [firstProduct] = Men_Products_Data;
   return (
     <div>
-        <div className='cart-upper-line'></div>
-        <div className='cart-main'>
-            <div className='cart-products-section'>
-                <div className='cart-products-img'><img src={firstProduct.imageLink1} alt="" /></div>
-                <div className='cart-products-details'>
-                    <h3>Name</h3>
-                    <div className='cart-products-size'>Size</div>
-                    <div className='cart-products-price'>258</div>
-                    <div className='cart-quanty-section'>
-                        <button>-</button>
-                        <div>number</div>
-                        <button>+</button>
-                    </div>
-                </div>
+      <div className="cart-upper-line"></div>
+      <div className="cart-main">
+        <div className="cart-products-section">
+          <div className="cart-products-img">
+            <img src={firstProduct.imageLink1} alt="" />
+          </div>
+          <div className="cart-products-details">
+            <h3>Name</h3>
+            <div className="cart-products-size">Size</div>
+            <div className="cart-products-price">258</div>
+            <div className="cart-quanty-section">
+              <button>-</button>
+              <div>number</div>
+              <button>+</button>
             </div>
-            <div className='cart-payment-section'></div>
+          </div>
         </div>
+        <div className="cart-payment-section">
+          <h2>Payment</h2>
+          <div className="cart-payment-section-price">Total Price: <span>3131</span></div>
+          <div className="cart-payment-section-payment-options">
+            <div>
+                <input type="radio" id="COD" name="fav_language" value="COD" />
+                <label for="COD">COD</label>
+            </div>
+            <div>
+                <input type="radio" id="CARD" name="fav_language" value="CARD" />
+                <label for="CARD">CARD</label>
+            </div>
+          </div>
+          <div className="cart-payment-section-checkout">
+            <button>Checkout</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
